08cd6a3209ee9c95ffd46bcf2b3cb932550a663a
Ahmed Hlilioune <a7lilion3@gmail.com>
2024-05-20 03:03:00 +0100

    Exercise 1

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Guess() {
+  const [label, setLabel] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(label);
+
+    setLabel("");
+  }
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={label}
+        onChange={(e) => setLabel(e.target.value.toUpperCase())}
+        pattern="[a-zA-Z]{5}"
+      />
+    </form>
+  );
+}
+
+export default Guess;
